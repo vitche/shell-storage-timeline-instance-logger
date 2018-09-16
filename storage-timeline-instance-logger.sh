8239319c602b3b82d0ab8263509df7eea08ad88a
Vitche Research Team Developer <developer@vitche.com>
2018-09-16 19:39:02 +0300

    1. A branch for installing the script.

--- storage-timeline-instance-logger.sh
+++ storage-timeline-instance-logger.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+case $1 in
+install)
+    wget https://raw.githubusercontent.com/vitche/shell-storage-timeline-instance-logger/master/storage-timeline-instance-logger.sh -O- > /usr/bin/storage-timeline-instance-logger.sh
+    chmod +x /usr/bin/storage-timeline-instance-logger.sh
+    exit 0
+esac
+
 CPU_USAGE=`top -bn2 | awk '/^top -/ { p=!p } { if (!p) print }' | grep "Cpu" | awk '{print $2 + $4}' | awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }'`
 # echo "$CPU_USAGE"
 storage-timeline.sh $1 addNumber $2 $3 "$CPU_USAGE"
