2d0950045715b5b57a1bdad93bc7174185829256
Vitche Research Team Developer <developer@vitche.com>
2018-10-12 18:45:01 +0300

    ))

--- storage-timeline-instance-logger.sh
+++ storage-timeline-instance-logger.sh
@@ -26,15 +26,15 @@ install-cron)
     fi
     exit 0
     ;;
-esac
-
-case $4 in
-cpu)
-    CPU_USAGE=`top -bn2 | awk '/^top -/ { p=!p } { if (!p) print }' | grep "Cpu" | awk '{print $2 + $4}' | awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }'`
-    # echo "$CPU_USAGE"
-    storage-timeline.sh $1 addNumber $2 $3 "$CPU_USAGE"
-    ;;
 *)
-    echo "Unable to track from unknown sensor $4"
-    ;;
+    case $4 in
+    cpu)
+        CPU_USAGE=`top -bn2 | awk '/^top -/ { p=!p } { if (!p) print }' | grep "Cpu" | awk '{print $2 + $4}' | awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }'`
+        # echo "$CPU_USAGE"
+        storage-timeline.sh $1 addNumber $2 $3 "$CPU_USAGE"
+        ;;
+    *)
+        echo "Unable to track from unknown sensor $4"
+        ;;
+    esac
 esac
